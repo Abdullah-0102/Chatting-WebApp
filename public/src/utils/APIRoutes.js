d19ab3671c561ff66b979d43f2b3b8928ca9489c
Abdullah Bin Imran <abi220419@gmail.com>
2024-10-13 14:49:36 +0500

    Updated .env file

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "http://ec2-13-60-87-122.eu-north-1.compute.amazonaws.com:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
